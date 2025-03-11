192e45b3707d56443bcfc2333828a061b135f7b4
harzh307 <harzh.patel@gmail.com>
2025-03-11 18:51:28 +0530

    commit code

--- src/api/admin/notification-template/[id]/route.ts
+++ src/api/admin/notification-template/[id]/route.ts
@@ -3,65 +3,63 @@ import { MedusaError } from "@medusajs/framework/utils";
 import NotificationTemplateModuleService from "../../../../modules/notification-template/service";
 import { NOTIFICATION_TEMPLATE_MODULE } from "../../../../modules/notification-template";
 type PostAdminUpdateNotificationTemplateType = {
-    template: string;
-    subject: string;
+  template: string;
+  subject: string;
 };
 export const GET = async (req: MedusaRequest, res: MedusaResponse) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    const query = req.scope.resolve("query")
-    const notificationTemplate = await query.graph({
-        entity: "notification_template",
-        fields: ["*", "event_id.*",],
-        filters: {
-            id
-        }
-    })
-    if (notificationTemplate.data[0]) {
-        return res.send(notificationTemplate.data[0]);
-    }
+  const query = req.scope.resolve("query");
+  const notificationTemplate = await query.graph({
+    entity: "notification_template",
+    fields: ["*", "event_id.*"],
+    filters: {
+      id,
+    },
+  });
+  if (notificationTemplate.data[0]) {
+    return res.send(notificationTemplate.data[0]);
+  }
 
-    throw new MedusaError(
-        MedusaError.Types.NOT_FOUND,
-        "Template not found."
-    );
-
+  throw new MedusaError(MedusaError.Types.NOT_FOUND, "Template not found.");
 };
 
 export const PUT = async (
-    req: MedusaRequest<PostAdminUpdateNotificationTemplateType>,
-    res: MedusaResponse
+  req: MedusaRequest<PostAdminUpdateNotificationTemplateType>,
+  res: MedusaResponse
 ) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    const notificationTemplateModuleService: NotificationTemplateModuleService = req.scope.resolve(NOTIFICATION_TEMPLATE_MODULE);
+  const notificationTemplateModuleService: NotificationTemplateModuleService =
+    req.scope.resolve(NOTIFICATION_TEMPLATE_MODULE);
 
-    const notificationTemplate = await notificationTemplateModuleService.updateNotificationTemplates({ id, ...req.body });
-    res.send({ notificationTemplate });
+  const notificationTemplate =
+    await notificationTemplateModuleService.updateNotificationTemplates({
+      id,
+      ...req.body,
+    });
+  res.send({ notificationTemplate });
 };
 
-
 export const DELETE = async (
-    req: MedusaRequest<PostAdminUpdateNotificationTemplateType>,
-    res: MedusaResponse
+  req: MedusaRequest<PostAdminUpdateNotificationTemplateType>,
+  res: MedusaResponse
 ) => {
-    const { id } = req.params;
-    const query = req.scope.resolve("query")
-    const notificationTemplateModuleService: NotificationTemplateModuleService = req.scope.resolve(NOTIFICATION_TEMPLATE_MODULE);
-    const notificationTemplate = await query.graph({
-        entity: "notification_template",
-        fields: ["*", "event_id.*",],
-        filters: {
-            id
-        }
-    })
-    if (notificationTemplate.data[0]) {
-        await notificationTemplateModuleService.deleteNotificationTemplates(id);
-        res.send();
-    }
+  const { id } = req.params;
+  const query = req.scope.resolve("query");
+  const notificationTemplateModuleService: NotificationTemplateModuleService =
+    req.scope.resolve(NOTIFICATION_TEMPLATE_MODULE);
+  const notificationTemplate = await query.graph({
+    entity: "notification_template",
+    fields: ["*"],
+    filters: {
+      id,
+    },
+  });
+  if (notificationTemplate.data[0]) {
+    await notificationTemplateModuleService.deleteNotificationTemplates(id);
+    res.send({});
+  }
 
-    throw new MedusaError(
-        MedusaError.Types.NOT_FOUND,
-        "Template not found."
-    );
+  throw new MedusaError(MedusaError.Types.NOT_FOUND, "Template not found.");
 };
